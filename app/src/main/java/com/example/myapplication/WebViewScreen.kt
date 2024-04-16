5d063743ffaf72cc6cc853ec46ac53e5ab20fe26
Victor Bodell <victor.bodell@sr.se>
2024-04-16 21:39:31 +0200

    Cleanup and reproduce website rendering

--- app/src/main/java/com/example/myapplication/WebViewScreen.kt
+++ app/src/main/java/com/example/myapplication/WebViewScreen.kt
@@ -1,19 +1,12 @@
-import android.view.ViewGroup.LayoutParams
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
 fun WebViewScreen(url: String) {
-    Box(modifier = Modifier.fillMaxSize()) {
         AndroidView(
-            factory = { WebView(it).apply {
-                layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-            } },
+            factory = { WebView(it) },
             update = { webView ->
                 webView.webViewClient = WebViewClient()
                 webView.settings.javaScriptEnabled = true
@@ -22,5 +15,4 @@ fun WebViewScreen(url: String) {
                 webView.loadUrl(url)
             }
         )
-    }
 }
